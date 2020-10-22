af51a02dfb73b66f9296484d039992741ef492a8
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-22 15:42:40 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    cart = cart
-    binding.pry
+
   end
 
   def add
-    @item = Item.find(params[:id])
+    binding.pry
     cart << @item.id
   end
 
